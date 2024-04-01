dfd9888f6e2c22a6d9adba0f9f95a8ade9585869
Colby Leavitt <colby.leavitt04@gmail.com>
2024-04-01 15:34:30 -0400

    initial commit - closes #4

---
+++ App.Canvas/Library.Canvas/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace Library.Canvas.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public Dictionary<int, double> Grades { get; set; }
+
+        public char Classification { get; set; }
+
+        public Person()
+        {
+            Grades = new Dictionary<int, double>();
+        }
+    }
+}
