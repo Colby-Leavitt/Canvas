2740301caddd145599897efe13d088471bfe549a
Colby Leavitt <colby.leavitt04@gmail.com>
2024-04-15 16:14:11 -0400

    Added Student derived class - Closes #41

--- App.Canvas/Library.Canvas/Models/Person.cs
+++ App.Canvas/Library.Canvas/Models/Person.cs
@@ -6,19 +6,17 @@
 
         public string Name { get; set; }
 
-        public Dictionary<int, double> Grades { get; set; }
 
-        public PersonClassification Classification { get; set; }
 
         public Person()
         {
             Name = string.Empty;
-            Grades = new Dictionary<int, double>();
+            
         }
 
         public override string ToString()
         {
-            return $"[{Id}] {Name} - {Classification}";
+            return $"[{Id}] {Name}";
         }
     }
 
