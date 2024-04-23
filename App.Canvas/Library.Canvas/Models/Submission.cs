f52838b880bb8d9a8d2050a27f7ebf76ae07f6c5
Colby Leavitt <colby.leavitt04@gmail.com>
2024-04-23 17:52:49 -0400

    Added Create submission functionality - Closes #44

---
+++ App.Canvas/Library.Canvas/Models/Submission.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Canvas.Models
+{
+    public class Submission
+    {
+        public int Id { get; private set; }
+        private static int lastId = 0;
+
+        public int StudentId { get; set; }
+        public int AssignmentId { get; set; }
+        public string Content { get; set; }
+
+
+        public Submission() 
+        {
+            Id = ++lastId;
+            Content = string.Empty;
+        }
+    }
+}
