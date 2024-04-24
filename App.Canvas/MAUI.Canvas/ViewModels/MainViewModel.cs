129834405f1fe2d3b8face23934098ca2e6e00f6
Colby Leavitt <colby.leavitt04@gmail.com>
2024-04-24 16:36:20 -0400

    Adding MAUI support for Canvas app

---
+++ App.Canvas/MAUI.Canvas/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Canvas.Models;
+
+namespace MAUI.Canvas.ViewModels
+{
+    internal class MainViewModel
+    {
+        public List<Person> students { get; set; } = new List<Person>();
+
+    }
+}
